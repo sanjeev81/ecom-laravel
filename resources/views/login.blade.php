6c3dae0f4635c76704e961486062727246e9e422
sanjeev kumar tiwari <sanjeevsgt@yahoo.co.in>
2020-12-01 20:44:17 +0530

    header,footer,login done

---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+@extends('master')
+@section('content')
+<h1>Login Page</h1>
+
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Password</label>
+      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+    </div>
+  </div>
+
+
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+
+
+@endsection
